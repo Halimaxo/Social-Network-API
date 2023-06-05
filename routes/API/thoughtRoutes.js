1b1b84ee53cd53a937552457edc827bc71f309f6
halimaxo <halimakshuchi@gmail.com>
2023-06-05 18:38:17 -0500

    thoughtControllers.js update

--- routes/API/thoughtRoutes.js
+++ routes/API/thoughtRoutes.js
@@ -1,1 +1,19 @@
 const express = require("express").Router();
+
+const {
+  getAllThoughts,
+  singleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+} = require("../../controllers/thoughtControllers");
+
+express.route("/").get(getAllThoughts).post(createThought);
+
+express
+  .route("/:thoughtId")
+  .get(singleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+module.exports = express;
