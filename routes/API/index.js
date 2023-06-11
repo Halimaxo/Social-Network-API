e8fa947569577683b789dccf8c31f5d833a4aac0
halimaxo <halimakshuchi@gmail.com>
2023-06-11 18:50:12 -0500

    post tutoring update

--- routes/API/index.js
+++ routes/API/index.js
@@ -1,7 +1,8 @@
 const userRoutes = require("./userRoutes");
-// const thoughtRoutes = require('./thoughtRoutes')
+const thoughtRoutes = require("./thoughtRoutes");
 const express = require("express").Router();
 
 express.use("/user", userRoutes);
+express.use("/thought", thoughtRoutes);
 
 module.exports = express;
